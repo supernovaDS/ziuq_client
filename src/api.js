037da2feb1c9a25f2a7b5dd322ac8bf911ef7025
supernovaDS <chakrabortysaibalik2323@gmail.com>
2026-03-30 16:30:11 +0530

    changed to prod url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const baseURLprod = 'https://ziuq-server.onrender.com/api';
 const baseURLdev = 'http://localhost:4000/api';
-const API = axios.create({ baseURL: baseURLdev });
+const API = axios.create({ baseURL: baseURLprod });
 
 // Add token to every request automatically
 API.interceptors.request.use((req) => {
